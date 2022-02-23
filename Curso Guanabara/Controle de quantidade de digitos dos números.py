dbac6e0a71ceb3a7b79f64cd10a2c3af9afa4585
Anderson Araújo <81644687+andersonssh@users.noreply.github.com>
2022-02-22 22:38:27 -0300

    Update Controle de quantidade de digitos dos números.py

--- Curso Guanabara/Controle de quantidade de digitos dos números.py
+++ Curso Guanabara/Controle de quantidade de digitos dos números.py
@@ -2,3 +2,11 @@ k = 'seila velio'
 for i in range(150): #{:x} diz quantos digitos o num vai ocupar independente do tamanho
     #print('{:3}'.format(i))
     print(f'{i:3}, testando string: {k:1}')
+    
+print("{:04d}".format(1)) # 0001
+print("{:04d}".format(323)) # 0323
+print("{:04d}".format(55555)) # 55555
+
+print("%04d" % (1)) # 0001
+
+print(f"{1:04d}") # 0001
