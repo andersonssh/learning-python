857dc11169ac58c3bf68837e5d38652a22b57838
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-03-14 13:26:27 -0300

    Adiciona enum e tipos opcionais. JSONSCHEMA

--- eu/validacoes de schemas/teste_de_schema_1.py
+++ eu/validacoes de schemas/teste_de_schema_1.py
@@ -9,10 +9,12 @@ conteudo = {
     'productName': 'jusenildson',
     'price': 1,
     'tags': ['a'],
+    'option': 'queijo',
     'dimensions': {
-        'lat': 10,
+        'lat': '10',
         'long': 90
     }
 }
+# a chave "option" tem valores predefinidos
 print(jsonschema.validate(instance=conteudo,
                           schema=schema_req))
